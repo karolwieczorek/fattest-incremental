deca15e6fe9439564948aa84e1e8c47df3bb6d69
Karol Wieczorek <karolwieczorek1992@gmail.com>
2025-03-29 17:28:57 +0100

    [wip]

--- Assets/_Code/FactoriesReferencer.cs
+++ Assets/_Code/FactoriesReferencer.cs
@@ -7,7 +7,7 @@ namespace FattestInc {
     public class FactoriesReferencer : HReferencer {
         const string DataPath = "_Data/Factories";
 
-        [AssetList(Path = DataPath, AutoPopulate = false)]
+        // [AssetList(Path = DataPath, AutoPopulate = false)]
         [SerializeField] List<FactoryLevelsData> factories;
 
         public IReadOnlyList<FactoryLevelsData> Factories => factories;
