8c3a5634f44b41fa2e29de695ef2bf280e3bf46a
Karol Wieczorek <karolwieczorek1992@gmail.com>
2025-03-29 17:22:26 +0100

    [wip]

--- Assets/_Code/FactoriesPanel.cs
+++ Assets/_Code/FactoriesPanel.cs
@@ -1,16 +1,20 @@
 using Hypnagogia.Utils;
 using UnityEngine;
+using Zenject;
 
 namespace FattestInc {
     public class FactoriesPanel : MonoBehaviour {
         [SerializeField] FactoryView factoryViewPrefab;
+        [SerializeField] GameObject clickerViewPrefab;
         [SerializeField] Transform content;
 
+        [HInject] DiContainer container;
         [HInject] FactoriesReferencer factoriesReferencer;
         [HInject] EconomyDataStore economyDataStore;
 
         void Start() {
             content.DestroyChildren();
+            container.InstantiatePrefab(clickerViewPrefab, content);
             foreach (var factoryLevelsData in factoriesReferencer.Factories) {
                 var factoryView = Instantiate(factoryViewPrefab, content);
                 factoryView.Init(factoryLevelsData, economyDataStore);
