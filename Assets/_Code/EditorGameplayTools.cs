4c31af627b2f3790ffd0dd48a62287e1fba78c41
Karol Wieczorek <karolwieczorek1992@gmail.com>
2025-04-17 15:14:52 +0200

    [wip]

---
+++ Assets/_Code/EditorGameplayTools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Hypnagogia.Utils;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace FattestInc {
+    public class EditorGameplayTools : MonoBehaviour {
+        [HInject] ResourceFactoriesHelper resourceFactoriesHelper;
+
+        [Button]
+        void IncrementMinutes(int minutes) {
+            resourceFactoriesHelper.TickAllFactories((float)TimeSpan.FromMinutes(minutes).TotalSeconds);
+        }
+
+        [Button]
+        void IncrementSeconds(float seconds) {
+            resourceFactoriesHelper.TickAllFactories(seconds);
+        }
+    }
+}
