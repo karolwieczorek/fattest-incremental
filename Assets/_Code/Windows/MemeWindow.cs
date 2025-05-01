77ec8f161a983c90ca56508a73cfe2129c9669fb
Karol Wieczorek <karolwieczorek1992@gmail.com>
2025-05-02 00:10:43 +0200

    memes window

---
+++ Assets/_Code/Windows/MemeWindow.cs
@@ -0,0 +1,12 @@
+﻿using FattestInc.Windows.General;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace FattestInc.Windows {
+    public class MemeWindow : SimpleWindow {
+        [SerializeField] TMP_Text titleLabel;
+        [SerializeField] Image image;
+        [SerializeField] TMP_Text descriptionLabel;
+    }
+}
