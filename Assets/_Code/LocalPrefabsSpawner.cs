ee6d6ea08223866547cd786d38b4d1664fe0bddb
Karol Wieczorek <karolwieczorek1992@gmail.com>
2025-05-05 07:51:11 +0200

    Local assets handling

---
+++ Assets/_Code/LocalPrefabsSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace FattestInc {
+    public class LocalPrefabsSpawner : MonoBehaviour {
+        public string resourceFolderPath = "LocalPrefabs";
+
+        void Start() {
+            var logicPrefabs = Resources.LoadAll<GameObject>(resourceFolderPath);
+            if (logicPrefabs.Length == 0) {
+                Debug.Log("No prefabs found in Resources/" + resourceFolderPath);
+                return;
+            }
+
+            foreach (GameObject prefab in logicPrefabs) {
+                var condition = prefab.GetComponent<SpawnCondition>();
+                if (condition != null) {
+                    if (condition.inDebug && !Debug.isDebugBuild)
+                        continue;
+
+                    if (condition.inRelease && Debug.isDebugBuild)
+                        continue;
+                }
+
+                Debug.Log($"Spawning Resources/{resourceFolderPath}/{prefab.name}");
+                GameObject instance = Instantiate(prefab, Vector3.zero, Quaternion.identity, transform);
+                instance.transform.localPosition = Vector3.zero;
+            }
+        }
+    }
+}
---
+++ Assets/_Code/SpawnCondition.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+namespace FattestInc {
+    public class SpawnCondition : MonoBehaviour {
+        public bool inDebug = true;
+        public bool inRelease = true;
+    }
+}
