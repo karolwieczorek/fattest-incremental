a1947fe3c84f5baf58492ff47cd2e33d69450a48
Karol Wieczorek <karolwieczorek1992@gmail.com>
2025-08-09 14:17:54 +0200

    changelog window [wip]

---
+++ Assets/_Code/Windows/ChangelogWindow.cs
@@ -0,0 +1,10 @@
+﻿using FattestInc.Windows.General;
+using TMPro;
+using UnityEngine;
+
+namespace FattestInc.Windows {
+    public class ChangelogWindow : SimpleWindow {
+        [SerializeField] TMP_Text descriptionLabel;
+        
+    }
+}
